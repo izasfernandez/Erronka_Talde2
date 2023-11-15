332c6e50667f52a78814659d4040f6faff62f26e
Sergioto275 <sgonzalezm22@fpsanjorge.com>
2023-11-15 11:55:15 +0100

    konpobaketa 17

--- Erronka/WES/Erabiltzaile_controller.php
+++ Erronka/WES/Erabiltzaile_controller.php
@@ -23,7 +23,7 @@
         }else{
             session_start();
             $erabil = new erabiltzailea();
-            echo $_SESSION["nan"];
+            echo "entra ".$_SESSION["nan"];
             $erabil->erabiltzailea_sesion_kargatu($_SESSION["nan"]);
             $json = json_encode($erabil);
             echo ($json);
