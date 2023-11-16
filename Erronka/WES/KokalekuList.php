95ae3deff4e17b43829c90bfd40359e169d1977e
Sergioto275 <sgonzalezm22@fpsanjorge.com>
2023-11-16 10:17:40 +0100

    Aldaketak

--- Erronka/WES/KokalekuList.php
+++ Erronka/WES/KokalekuList.php
@@ -58,7 +58,6 @@
             WHERE kokalekua.idGela = gela.id 
             AND inbentarioa.idEkipamendu = ekipamendua.id 
             AND inbentarioa.etiketa = kokalekua.etiketa".$filtro;
-            echo $sql;
             $this->informazioa_karga($sql);
         }
 
