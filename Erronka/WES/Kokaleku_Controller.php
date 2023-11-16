01097b4d6bc5cf06d3f2a96ee7dd3fca1e6529cd
Sergioto275 <sgonzalezm22@fpsanjorge.com>
2023-11-16 08:29:41 +0100

    Aldaketa

--- Erronka/WES/Kokaleku_Controller.php
+++ Erronka/WES/Kokaleku_Controller.php
@@ -2,6 +2,7 @@
     // header("Access-Control-Allow-Headers:{$_SERVER['HTTP_ACCESS_CONTROL_REQUEST_HEADERS']}");
     // header("Access-Control-Allow-Origin: {$_SERVER['HTTP_ORIGIN']}");
     header("Access-Control-Allow-Origin: *");
+    header("Access-Control-Allow-Headers: Content-Type");
     header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
     // header("Content-Type: application/json; charset=UTF-8");
 
