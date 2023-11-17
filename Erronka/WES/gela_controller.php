44a1387aab5990f2f01bf9594062533bc1428e55
Sergioto275 <sgonzalezm22@fpsanjorge.com>
2023-11-17 09:14:54 +0100

    ald

--- Erronka/WES/gela_controller.php
+++ Erronka/WES/gela_controller.php
@@ -64,8 +64,11 @@
         $json_data = file_get_contents("php://input");
         $data = json_decode($json_data,true);
         if (isset($data["kontsulta"])) {
+            echo "bbb";
             if (isset($data["gela"])) {
+                echo "aaa";
                 $exist = $gela->izenaExists($data["gela"]);
+                echo $exist;
                 $json = json_encode($exist);
             }
         }else{
