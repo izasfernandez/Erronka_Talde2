27fc225eba9b7c77e6785dace3393d83bdd35a6e
Sergioto275 <sgonzalezm22@fpsanjorge.com>
2023-11-20 09:16:22 +0100

    Bug erroreak

--- Erronka/WES/InbentarioList.php
+++ Erronka/WES/InbentarioList.php
@@ -72,7 +72,7 @@
          * @access public
          */
         function kokaleku_art_karga(){
-            $sql = "SELECT inbentarioa.etiketa, ekipamendua.izena, inbentarioa.erosketaData FROM inbentarioa, ekipamendua where (inbentarioa.etiketa NOT IN (SELECT kokalekua.etiketa FROM kokalekua) OR inbentarioa.etiketa IN (SELECT kokalekua.etiketa FROM kokalekua WHERE kokalekua.amaieraData IS NOT NULL AND kokalekua.amaieraData < curdate())) AND inbentarioa.idEkipamendu = ekipamendua.id";
+            $sql = "SELECT inbentarioa.etiketa, ekipamendua.izena, inbentarioa.erosketaData FROM inbentarioa, ekipamendua where (inbentarioa.etiketa NOT IN (SELECT kokalekua.etiketa FROM kokalekua)";
             $this->informazioa_karga($sql);
         }
 
